c5824d93885cbe2e74a167c09762f20621bb7a9c
Mtan20 <93356669+Mtan20@users.noreply.github.com>
2021-10-30 13:11:16 -0800

    Update MayleeTanNaotoOkaProjectThreeTask1.java

--- Project3/src/MayleeTanNaotoOkaProjectThreeTask1.java
+++ Project3/src/MayleeTanNaotoOkaProjectThreeTask1.java
@@ -4,11 +4,21 @@ public class MayleeTanNaotoOkaProjectThreeTask1
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		
-		Hello Maylee don't block me please
+			
 
+	}
+	
+	// Task #2
+	public static double[] celsius(double[] fahrenheit1)
+	{
+		double[] celsius = new double[5];
+ 
+		for(int i = 0; i < fahrenheit1.length; i++)
+		{
+			celsius[i] = (5.0 / 9) * (fahrenheit1[i] - 32);
+		}
+		
+		return celsius;
 	}
 
 }
